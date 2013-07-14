6397705256ec59aebca8aa76957b0326541be243
AlexMog <AlexMog@AlexMog-PC>
2013-07-14 22:09:05 +0200

    First commit

---
+++ JavaMogLib/src/org/MogLib/library/utils/Downloader.java
@@ -0,0 +1,98 @@
+package org.MogLib.library.utils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.swing.JProgressBar;
+
+public class Downloader {
+	/**
+	 * Download Methods
+	 * 
+	 * @author AlexMog (http://alexmog.tv/)
+	 */
+	private Downloader() {/* Don't allow the instantiation of the class */};
+	
+	/**
+	 * Used to download file from url to destination
+	 * 
+	 * @param url The url of the file to download
+	 * @param destination The destination of the file to download
+	 * @param progressBar A progress bar to see the progression download of the file
+	 * @throws Exception
+	 */
+	public static void downloadFile(String sUrl, String destination, JProgressBar progressBar, Proxy proxy) throws Exception {
+		URL url = new URL(sUrl.replace(" ", "%20"));
+		URLConnection conn;
+		InputStream is;
+		File f;
+		File d;
+		FileOutputStream fos;
+		byte[] buff = new byte[1024];
+		int n = 0;
+		int fileSize = 0;
+		
+		if (proxy == null)
+			conn = url.openConnection();
+		else
+			conn = url.openConnection(proxy);
+		conn.connect();
+		
+		fileSize = conn.getContentLength();
+		if (fileSize == -1)
+			throw new Exception("Impossible to open the file " + sUrl + " - 404 Error certainly");
+		
+		if (progressBar != null)
+			progressBar.setMaximum(progressBar.getMaximum() + fileSize);
+		
+		is = new BufferedInputStream(url.openStream());
+		
+		f = new File(destination);
+    	if (!f.exists()) {
+    		d = f.getParentFile();
+    		if (d != null && !d.exists())
+    			d.mkdirs();
+    		f.createNewFile();
+    	} else
+    		f.delete();
+        fos = new FileOutputStream(destination);
+        
+        while ((n = is.read(buff)) != -1) {
+            fos.write(buff, 0, n);
+            if (progressBar != null)
+            	progressBar.setValue(progressBar.getValue() + n);
+        }
+    	
+        fos.flush();
+    	fos.close();
+    	is.close();
+	}
+	
+	/**
+	 * Used to download file from url to destination
+	 * 
+	 * @param url The url of the file to download
+	 * @param destination The destination of the file to download
+	 * @throws Exception
+	 */
+	public static void downloadFile(String url, String destination) throws Exception {
+		downloadFile(url, destination, null, null);
+	}
+	
+	/**
+	 * Used to download file from url to destination
+	 * 
+	 * @param url The url of the file to download
+	 * @param destination The destination of the file to download
+	 * @param progressBar A progressBar to the file progression
+	 * @throws Exception
+	 */
+	public static void downloadFile(String url, String destination, JProgressBar progressBar) throws Exception {
+		downloadFile(url, destination, progressBar, null);
+	}
+}
---
+++ JavaMogLib/src/org/MogLib/library/utils/HashFileSum.java
@@ -0,0 +1,82 @@
+package org.MogLib.library.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+
+public class HashFileSum {
+	/**
+	 * HashFile methods
+	 * Thoes methods are not mine, thx to Mr-Hide to have done it.
+	 *
+	 * @author Mr-Hide (JB, http://www.mrhide.fr), Adapted by AlexMog (http://alexmog.tv/)
+	 */
+	private HashFileSum() { /* Don't allow the instantiation of this class */};
+	
+	/**
+	 * Returns the SHA-1 encrypton of the file
+	 * 
+	 * @param File file The file who you want the SHA-1 encrypton
+	 * @return The SHA-1 encrypton
+	 * @throws Exception 
+	 */
+	public static String sha1sum(File file) throws Exception{
+		String localSha1Sum = null;
+		
+		if (file.exists() && file.isFile() && file.canRead()) {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			@SuppressWarnings("resource")
+			DigestInputStream dis = new DigestInputStream(new FileInputStream(file), md);
+			dis.on(true);
+			
+			while (dis.read() != -1);
+
+			byte[] b = md.digest();
+			localSha1Sum = getHexString(b);
+		} else
+			throw new Exception("Impossible to open the file " + file.getAbsolutePath());
+	    return (localSha1Sum);
+	}
+	
+	/**
+	 * Returns the MD5 encrypton of the file
+	 * 
+	 * @param File file the file you want to know the MD5 encrypton
+	 * @return The MD5 encrypton of the file
+	 * @throws Exception 
+	 */
+	public static String md5sum(File file) throws Exception {
+	    String localMd5Sum = null;
+	    
+	    if (file.exists() && file.isFile() && file.canRead()){
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			@SuppressWarnings("resource")
+			DigestInputStream dis = new DigestInputStream(new FileInputStream(file), md);
+			dis.on(true);
+
+			while (dis.read() != -1);
+			byte[] b = md.digest();
+			localMd5Sum = getHexString(b);
+	    } else
+	    	throw new Exception("Impossible to open the file " + file.getAbsolutePath());
+	    return (localMd5Sum);
+	}
+	
+	/**
+	 * Used to convert the byte value to Hex value
+	 * 
+	 * @param bytes the bytes
+	 * @return HexValue calculated
+	 */
+	private static String getHexString(byte[] bytes) {
+		StringBuilder sb = new StringBuilder(bytes.length * 2);
+		for (byte b : bytes) {
+			if (b <= 0x0F && b >= 0x00) {
+				sb.append('0');
+	        }
+			sb.append( String.format("%x", b) );
+		}
+		return sb.toString();
+	}
+}
