933dbc1a111474bd27d46741eb35582183c9afff
AlexMog <AlexMog@AlexMog-PC>
2013-07-16 14:45:19 +0200

    Added moveDir/moveFile methods

--- JavaMogLib/src/org/MogLib/library/utils/DirUtils.java
+++ JavaMogLib/src/org/MogLib/library/utils/DirUtils.java
@@ -31,7 +31,7 @@ public class DirUtils {
 				    listFiles.addAll(readDir(list[i]));
 		    } else
 		    	throw new Exception("File read error");
-		} else if (!path.isDirectory()) {
+		} else {
 		    currentFilePath = path.getAbsolutePath();
 		    listFiles.add(currentFilePath);
 		}
@@ -39,10 +39,10 @@ public class DirUtils {
     }
 	
 	/**
-	 * Used to copy a dir
+	 * Used to copy a directory
 	 * 
-	 * @param dir The dir to copy
-	 * @param destination The destination of the dir
+	 * @param dir The directory to copy
+	 * @param destination The destination of the directory
 	 * @throws Exception
 	 */
 	public static void copyDir(File dir, File destination) throws Exception {
@@ -59,6 +59,30 @@ public class DirUtils {
 			if (!d.exists())
 				d.mkdirs();
 			FileUtils.copyFile(new File(sFile), file);
+		}
+	}
+	
+	/**
+	 * Used to move a directory
+	 * 
+	 * @param dir The directory to move
+	 * @param destination The destination of the directory
+	 * @throws Exception
+	 */
+	public static void moveDir(File dir, File destination) throws Exception {
+		ArrayList<String> files = readDir(dir);
+		File file;
+		File d;
+		
+		if (!dir.isDirectory() || !destination.isDirectory())
+			throw new Exception("Dir or Destination is not a directory.");
+		
+		for(String sFile : files) {
+			file = new File(destination.getAbsolutePath() + sFile.replace(dir.getAbsolutePath(), ""));
+			d = file.getParentFile();
+			if (!d.exists())
+				d.mkdirs();
+			FileUtils.moveFile(new File(sFile), file);
 		}
 	}
 }
--- JavaMogLib/src/org/MogLib/library/utils/FileUtils.java
+++ JavaMogLib/src/org/MogLib/library/utils/FileUtils.java
@@ -63,4 +63,53 @@ public class FileUtils {
 	public static void copyFile(File file, File destination) throws IOException {
 		copyFile(file, destination, null);
 	}
+	
+	/**
+	 * Used to move file to destination
+	 * 
+	 * @param file The file to move
+	 * @param destination The destination where he will be moved
+	 * @param progressBar A progress bar to see the progression of the file
+	 * @throws IOException 
+	 */
+	public static void moveFile(File file, File destination, JProgressBar progressBar) throws IOException {
+		InputStream is;
+		OutputStream os;
+		byte[] buff = new byte[1024];
+		int readed = 0;
+		
+		if (file.isDirectory()) {
+			destination.mkdirs();
+			return ;
+		}
+		
+		is = new FileInputStream(file);
+		os = new FileOutputStream(destination);
+		
+		if (progressBar != null)
+			progressBar.setMaximum((int) (progressBar.getMaximum() + file.length()));
+		
+		while ((readed = is.read(buff)) != -1) {
+			os.write(buff);
+			if (progressBar != null)
+				progressBar.setValue(progressBar.getValue() + readed);
+		}
+		
+		file.delete();
+		
+		os.flush();
+		os.close();
+		is.close();
+	}
+	
+	/**
+	 * Used to move file to destination
+	 * 
+	 * @param file The file to move
+	 * @param destination The destination where he will be moved
+	 * @throws IOException A progress bar to see the progression of the file
+	 */
+	public static void moveFile(File file, File destination) throws IOException {
+		moveFile(file, destination, null);
+	}
 }
